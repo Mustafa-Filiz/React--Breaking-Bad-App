f725481bd5e7dcece0e849512a3829885544a5db
Mustafa Filiz <mustafafiliz18@gmail.com>
2021-11-22 23:57:46 +0300

    updates

--- src/pages/Quotes.js
+++ src/pages/Quotes.js
@@ -18,20 +18,28 @@ function Quotes() {
     console.log(quotes);
 
     useEffect(() => {
-        dispatch(fetchQuotes());
-    }, [dispatch]);
+        if (status === 'idle') {
+            dispatch(fetchQuotes());
+        }
+    }, [dispatch, status]);
 
     if (error) {
         return <h3>{error}</h3>;
     }
 
     return (
-        <div style={{ textAlign: 'left', padding: 20 }}>
+        <div style={{ textAlign: 'left', padding: '0 20px' }}>
+            <h1>Quotes</h1>
             {status === 'loading' && <h2>Loading...</h2>}
             {status === 'succeeded' &&
                 quotes.map((quote) => (
                     <Quote key={quote.quote_id} quote={quote} />
                 ))}
+            {status === 'succeeded' && (
+                <div style={{ textAlign: 'center' }}>
+                    {quotes.length} quotes
+                </div>
+            )}
         </div>
     );
 }
