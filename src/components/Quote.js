2d01094e388de8f2d2356e438c89080d83e46420
Mustafa Filiz <mustafafiliz18@gmail.com>
2021-11-22 23:20:02 +0300

    updates

---
+++ src/components/Quote.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Quote({ quote }) {
+    return (
+        <div style={{padding: "10px 0"}}>
+            <q>{quote.quote}</q> <strong>{quote.author}</strong>
+        </div>
+    );
+}
+
+export default Quote;
--- src/pages/Quotes.js
+++ src/pages/Quotes.js
@@ -1,11 +1,39 @@
-import React from 'react'
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { useSelector } from 'react-redux';
+import Quote from '../components/Quote';
+import {
+    errorSelector,
+    fetchQuotes,
+    quotesSelector,
+    statusSelector,
+} from '../redux/quotesSlice';
 
 function Quotes() {
-	return (
-		<div>
-			Quotes
-		</div>
-	)
+    const dispatch = useDispatch();
+    const quotes = useSelector(quotesSelector);
+    const status = useSelector(statusSelector);
+    const error = useSelector(errorSelector);
+
+    console.log(quotes);
+
+    useEffect(() => {
+        dispatch(fetchQuotes());
+    }, [dispatch]);
+
+    if (error) {
+        return <h3>{error}</h3>;
+    }
+
+    return (
+        <div style={{ textAlign: 'left', padding: 20 }}>
+            {status === 'loading' && <h2>Loading...</h2>}
+            {status === 'succeeded' &&
+                quotes.map((quote) => (
+                    <Quote key={quote.quote_id} quote={quote} />
+                ))}
+        </div>
+    );
 }
 
-export default Quotes
+export default Quotes;
